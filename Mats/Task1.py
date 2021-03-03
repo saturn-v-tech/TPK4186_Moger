11b3bdf7697f877129d1f4cddbbf6538f53a0e21
Mats <matsemos@stud.ntnu.no>
2021-03-03 10:44:06 +0100

    bare test med start på task1

---
+++ Mats/Task1.py
@@ -0,0 +1,50 @@
+#Task 1
+
+
+# Encode Nodes
+#-------------
+
+class Nodes():
+  
+  def __init__(self, node):
+    self.node = node
+
+
+  def GetNode(self):
+    return self.node
+
+  # def SetNode(self):
+  #   self.node = node
+    
+
+
+a = Nodes("k1")
+b = Nodes("k3")
+
+# # c = Nodes()
+
+# print(a.node)
+
+
+print(Nodes.GetNode(b) + ' and ' + Nodes.GetNode(a))
+# print(Nodes.self.values())
+
+
+
+
+
+
+# Encode Edges
+#-------------
+
+
+  # class edges():
+    
+
+
+
+
+
+
+
+
